5e7a6dbc9e51211f23db905a6481688e8151d10f
keenancrawford <kpcrawford12@gmail.com>
2020-07-20 19:00:42 -0500

    Database Connection code for AWS MySQL instance

---
+++ src/testgui/AWSConnection.java
@@ -0,0 +1,23 @@
+package testgui;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class AWSConnection {
+    String url = "jdbc:mysql://mysqlinstance.cmlrtubxh7mp.us-east-2.rds.amazonaws.com:3306/Team5DB";
+    String user = "admin";
+    String password = "Skating1";
+    Connection conn = null;
+
+    public void dbConnection(){
+        try {
+
+            conn = DriverManager.getConnection(url,user,password);
+            System.out.println("Database Connected");
+        }catch (SQLException e ){
+            e.printStackTrace();
+            System.out.println("Connection Failed " + e.getLocalizedMessage());
+        }
+    }
+}
